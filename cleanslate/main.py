b7db936f1c28a542534624da386bc714ca328dc2
Malcolm R. Jones <maljonesdev@gmail.com>
2021-10-12 14:34:23 -0400

    Add gitignore

--- cleanslate/main.py
+++ cleanslate/main.py
@@ -1,10 +1,12 @@
 from cleanslate import CleanSlateHandler
 
 text = """
-This fucking planet is hell.
+Good fuckin shit it’s nice to see some good social content on this sub. Seems like everybody’s complaining about not having friends or not enjoying their on campus experience.
 """
 
 handler = CleanSlateHandler()
 censorPairs = handler.searchInappropriateWordsInText(text)
 censoredText = handler.censorText(text, censorPairs)
+
+print(text + "\n\n")
 print(censoredText)
