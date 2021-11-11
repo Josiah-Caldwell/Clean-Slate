564ef17cd7ad2805e5b5aefcb60b8b24373345ba
mileswalker51 <33327691+mileswalker51@users.noreply.github.com>
2021-11-10 20:11:11 -0500

    Malcolm updates

--- cleanslate/cleanslate.py
+++ cleanslate/cleanslate.py
@@ -1,14 +1,27 @@
 import re
 import csv
+import urllib
+from bs4 import BeautifulSoup
 
 class CleanSlateHandler:
+    def parseText(self):
+        # provide url
+        url = 'https://www.reddit.com/r/CasualConversation/comments/pt93vf/if_i_win_the_lottery_what_would_you_like/'
+
+        # open url
+        html = urllib.request.urlopen(url)
+
+        # parse file
+        parsedText = BeautifulSoup(html, 'html.parser')
+
+        return parsedText
+
     def __init__(self):
         self.INAPPROPRIATE_WORD_LIST = []
         with open("../data/bad-words.csv") as csvfile:
             reader = csv.reader(csvfile)
             for row in reader:
                 self.INAPPROPRIATE_WORD_LIST += row
-
 
     def searchInappropriateWordsInText(self, text):
         censorPairs = []
