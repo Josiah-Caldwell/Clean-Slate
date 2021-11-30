68b12e0df1c6445b574336b26aa1eb7f0cabd254
Josiah-Caldwell <jjoec321@gmail.com>
2021-11-30 15:30:26 -0500

    Get Title
    Functional getTitle method
    Skeleton of GetComments, Submission, postCensored comments, title, submission methods

---
+++ apiCall.js
@@ -0,0 +1,124 @@
+//Consistent variable for pathways
+var url = 'https://eqtk61pp5h.execute-api.us-east-1.amazonaws.com/prod';
+var titleEndPoint = '/titles';
+var commentsEndPoint = '/comment';
+var postContentEndPoint = '/submission';
+var tabUrl; 
+
+//Get url of current tab
+function getTabUrl() {
+    console.log("Get Tab Url");
+    return chrome.tabs.query({
+        active: true,
+        lastFocusedWindow: true
+    });
+}
+
+//Fill Out
+function getCommentID() {
+
+}
+
+function getTitles() {
+    getTabUrl()
+        .then(tabs => {
+            console.log("Get Title")
+            var tab = tabs[0];
+            console.log(tab.url);
+            tabUrl = tab.url;
+            console.log("URL: " + tabUrl)
+            return tabUrl;
+        })
+        .then(tabUrl => {
+            console.log("API Call with URL: " + tabUrl)
+            return fetch(url + titleEndPoint, {
+                method: 'POST',
+                header: {
+                    "Origin": 'text/plain',
+                    "Content-Type": 'application/json'
+                },
+                body: JSON.stringify({
+                    "URL": tabUrl
+                })
+            });
+        })
+        .then(response => {
+            response.json().then(responseJson => {
+                console.log(responseJson)
+            })
+        });
+
+}
+
+//Format
+function getComments() {
+    getTabUrl()
+        .then(tabs => {
+            console.log("Get Title")
+            var tab = tabs[0];
+            console.log(tab.url);
+            tabUrl = tab.url;
+            console.log("URL: " + tabUrl)
+            return tabUrl;
+        })
+        .then(tabUrl => {
+            console.log("API Call with URL: " + tabUrl)
+            return fetch(url + commentsEndPoint, {
+                method: 'POST',
+                header: {
+                    "Origin": 'text/plain',
+                    "Content-Type": 'application/json'
+                },
+                body: JSON.stringify({
+                    "URL": tabUrl
+                    "commentID" : //ID of comment changed
+                })
+            });
+        })
+        .then(response => {
+            response.json().then(responseJson => {
+                console.log(responseJson)
+            })
+        });
+
+}
+
+//Format
+function getTitles() {
+    getTabUrl()
+        .then(tabs => {
+            console.log("Get Title")
+            var tab = tabs[0];
+            console.log(tab.url);
+            tabUrl = tab.url;
+            console.log("URL: " + tabUrl)
+            return tabUrl;
+        })
+        .then(tabUrl => {
+            console.log("API Call with URL: " + tabUrl)
+            return fetch(url + postContentEndPoint, {
+                method: 'POST',
+                header: {
+                    "Origin": 'text/plain',
+                    "Content-Type": 'application/json'
+                },
+                body: JSON.stringify({
+                    "URL": tabUrl
+                })
+            });
+        })
+        .then(response => {
+            response.json().then(responseJson => {
+                console.log(responseJson)
+            })
+        });
+
+}
+
+getTitles();
+
+//Change Webpage title with API Response JSON obj
+document.getElementsByClassName("title");
+
+//Change Webpage Comments based on associated ID
+//Change Submission Content
